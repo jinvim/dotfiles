f339fc8991b454af75df5bf81ed64f16980b58d1
Seonjin Lee <sjbase@me.com>
2025-07-26 12:37:36 -0400

    minimal quarto

--- nvim/.config/nvim/lua/plugins/lang/quarto.lua
+++ nvim/.config/nvim/lua/plugins/lang/quarto.lua
@@ -1,57 +1,47 @@
-return {
-  {
-    "jmbuhr/otter.nvim",
-    opts = {
-      buffers = {
-        set_filetype = true,
-        write_to_disk = true,
-      },
-    },
-    enabled = function()
-      -- Only enable this plugin on macOS
-      return vim.loop.os_uname().sysname == "Darwin"
-    end,
-    ft = { "quarto" },
-  },
-  {
-    "quarto-dev/quarto-nvim",
-    cmd = { "QuartoActivate", "QuartoPreview" },
-    dependencies = {
+if vim.loop.os_uname().sysname == "Darwin" then
+  return {
+    {
       "jmbuhr/otter.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "neovim/nvim-lspconfig",
-    },
-    opts = {
-      codeRunner = {
-        enabled = true,
-        default_method = "iron",
-        never_run = { "yaml" },
+      opts = {
+        buffers = {
+          set_filetype = true,
+          write_to_disk = true,
+        },
       },
+      ft = { "quarto" },
     },
-    enabled = function()
-      -- Only enable this plugin on macOS
-      return vim.loop.os_uname().sysname == "Darwin"
-    end,
-    ft = { "quarto" },
-  },
-  {
-    "jmbuhr/telescope-zotero.nvim",
-    dependencies = {
-      {
-        "kkharji/sqlite.lua",
-        "nvim-telescope/telescope.nvim",
+    {
+      "quarto-dev/quarto-nvim",
+      cmd = { "QuartoActivate", "QuartoPreview" },
+      dependencies = {
+        "jmbuhr/otter.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "neovim/nvim-lspconfig",
       },
+      opts = {
+        codeRunner = {
+          enabled = true,
+          default_method = "iron",
+          never_run = { "yaml" },
+        },
+      },
+      ft = { "quarto" },
     },
-    opts = {},
-    config = function(_, opts)
-      require("zotero").setup(opts)
-      local telescope = require("telescope")
-      telescope.load_extension("zotero")
-    end,
-    enabled = function()
-      -- Only enable this plugin on macOS
-      return vim.loop.os_uname().sysname == "Darwin"
-    end,
-    ft = { "quarto" },
-  },
-}
+    {
+      "jmbuhr/telescope-zotero.nvim",
+      dependencies = {
+        {
+          "kkharji/sqlite.lua",
+          "nvim-telescope/telescope.nvim",
+        },
+      },
+      opts = {},
+      config = function(_, opts)
+        require("zotero").setup(opts)
+        local telescope = require("telescope")
+        telescope.load_extension("zotero")
+      end,
+      ft = { "quarto" },
+    },
+  }
+end
