cd43201801b3cdd0b837af5f0ff594db5838d004
Seonjin Lee <sjbase@me.com>
2025-07-26 13:21:29 -0400

    a helper function for saving main qmd file

--- nvim/.config/nvim/after/ftplugin/quarto.lua
+++ nvim/.config/nvim/after/ftplugin/quarto.lua
@@ -14,4 +14,33 @@ if not vim.g.vscode then
   end, { desc = "run all cells of all languages", silent = true })
   vim.keymap.set("n", "<leader>nr", ":Telescope zotero<cr>", { desc = "zotero citations", silent = true })
   vim.keymap.set({ "n", "i", "v" }, "<M-r>", "<cmd>Telescope zotero<cr>", { desc = "zotero citations", silent = true })
+
+  -- function that retrrievs the buffer name of "manuscript.qmd" and saves it, and comback to the current buffer
+  -- because quarto only triggers preview refresh on save of the main file
+  local function write_main()
+    local open_files = vim.api.nvim_list_bufs()
+
+    if not vim.g.quarto_main_bufnr then
+      local main_bufnr = vim
+        .iter(open_files)
+        :map(function(buf)
+          local name = vim.api.nvim_buf_get_name(buf)
+          return name:match("manuscript.qmd") and buf or nil
+        end)
+        :find(function(buf)
+          return buf ~= nil
+        end)
+      vim.g.quarto_main_bufnr = main_bufnr
+    end
+
+    if vim.g.quarto_main_bufnr then
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.api.nvim_set_current_buf(vim.g.quarto_main_bufnr)
+      vim.api.nvim_command("write")
+      vim.api.nvim_set_current_buf(bufnr)
+    else
+      vim.notify("no bufnr found for manuscript.qmd; maybe it's not open?", vim.log.levels.INFO)
+    end
+  end
+  vim.keymap.set("n", "<leader>nw", write_main, { desc = "write manuscript.qmd", silent = true })
 end
