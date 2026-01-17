88bf1af8c2e61b1c0a5e528f0d09ee446b9a54d4
Seonjin Lee <sjbase@me.com>
2026-01-17 12:54:16 -0500

    treesitter update config

--- nvim/.config/nvim/lua/plugins/term/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/term/treesitter.lua
@@ -1,32 +1,28 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
     build = ":TSUpdate",
-    opts = function()
-      local ts = require("nvim-treesitter.configs")
-      ts.setup({
-        ignore_install = {},
-        ensure_installed = {},
-        highlight = {
-          enable = true,
-          disable = { "stata" },
-          additional_vim_regex_highlighting = {
-            "vimdoc",
-            "luadoc",
-            "vim",
-            "lua",
-            "markdown",
-          },
+    opts = {
+      ignore_install = {},
+      ensure_installed = {},
+      highlight = {
+        enable = true,
+        disable = { "stata" },
+        additional_vim_regex_highlighting = {
+          "vimdoc",
+          "luadoc",
+          "vim",
+          "lua",
+          "markdown",
         },
-        indent = {
-          enable = true,
-        },
-        autotag = {
-          enable = true,
-        },
-        auto_install = true,
-      })
-    end,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      auto_install = true,
+    },
   },
 }
